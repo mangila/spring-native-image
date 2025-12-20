6aa4e397b1f6b83fd2f737c907e1ff19ad4c8554
Erik Olsson <olsson.erik1993@gmail.com>
2025-12-20 18:18:47 +0100

    web flux cloud fn

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # spring-native-image
 
-Spring GraalVM native image as FaaS in k8s
-
-Spring native image compile
+Spring GraalVM native image as a Function in k8s (Minikube) with Fission
 
 - Serverless deployed with [fission.io](https://fission.io/)
 
-#### Fission
+#### Minikube
+
+Make sure Minikube is running.
 
 ```bash
 docker build -t spring-native-image .
 ```
 
 ```bash
-fission fn run-container --name hello --image spring-native-imagefission route create --name hello-route --function hello --prefix /hello --keepprefix --port 8888
+minikube image load spring-native-image
+```
+
+#### Fission
+
+Make sure Fission CLI is installed and running.
+
+```bash
+fission version && fission check
+```
+
+```bash
+fission fn run-container --name hello --image spring-native-image
 ```
 
 ```bash
--- native-image/src/main/java/com/github/mangila/nativeimage/Application.java
+++ native-image/src/main/java/com/github/mangila/nativeimage/Application.java
@@ -2,20 +2,19 @@ package com.github.mangila.nativeimage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from Fission as GraalVM Native Image!";
+    @Bean
+    public Supplier<String> hello() {
+        return () -> "Hello from Spring Native";
     }
-
 }
